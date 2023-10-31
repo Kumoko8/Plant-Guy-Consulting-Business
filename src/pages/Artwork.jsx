c6622dc53279c88c001c77b056bbee40fd07ebec
Kumori <tauen8@gmail.com>
2023-10-31 06:59:31 -0500

    added grid to artwork headings

--- src/pages/Artwork.jsx
+++ src/pages/Artwork.jsx
@@ -1,28 +1,50 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Artwork() {
   return (
     <div>
-    <h1>Artwork</h1>
+      <Grid container >
+      <h1>Artwork</h1>
+        <Grid xs={12}>
 
-    <h2>Wire Frames</h2>
-    <h2>Logos</h2>
-    <h2>Characters</h2>
-    <h2>Designs</h2>
-    <h2>Animation</h2>
-    <h2>Portraits</h2>
-    <h2>Stories/Scenes</h2>
+          <h2>Wire Frames</h2>
+        </Grid>
+        <Grid xs={12}>
+
+          <h2>Logos</h2>
+        </Grid>
+        <Grid xs={12}>
+
+          <h2>Characters</h2>
+        </Grid>
+        <Grid xs={12}>
+          <h2>Designs</h2>
+
+        </Grid >
+        <Grid xs={12}>
+          <h2>Animation</h2>
+
+        </Grid>
+        <Grid xs={12}>
+          <h2>Portraits</h2>
+
+        </Grid>
+        <Grid xs={12}>
+
+          <h2>Stories/Scenes</h2>
+        </Grid>
+      </Grid>
 
 
 
 
-    <Link to="/Projects">
-    <button >
-      Back
-    </button>
-    </Link>
-  </div>
+      <Link to="/Projects">
+        <button >
+          Back
+        </button>
+      </Link>
+    </div>
 
-);
+  );
 
 }
