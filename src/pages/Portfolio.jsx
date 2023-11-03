8ff6e224034e1de7b9912ef687f89d74308ed4a5
Kumori <tauen8@gmail.com>
2023-11-03 11:28:48 -0500

    removed collectibles app and replaced it with fitness design

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,7 +7,14 @@ export default function Portfolio() {
                 <h1> Portfolio</h1>
             </div>
             <div> <h3>Click each image to view the app!</h3></div>
-           
+            <section>
+                <a href="../../Fitness-Tracker.pdf" target="_blank">
+                    <img className="image-deco" src='../../running.png'></img>
+                </a>
+            </section>
+            <section>
+            <button>Fitness Tracker Design</button>
+            </section>
             <section>
                 <a href="https://kumoko8.github.io/Front-End-App-DnD-Character-Project/" target="_blank">
                     <img className="image-deco" src='../../DnD.png'></img>
@@ -15,14 +22,6 @@ export default function Portfolio() {
             </section>
             <section>
             <a href="https://github.com/Kumoko8/Front-End-App-DnD-Character-Project.git"target="_blank"><button>DnD Character Generator</button></a>
-            </section>
-            <section>
-            <a href="https://project-collections-cfb3d3b9c8f8.herokuapp.com/"target="_blank">
-                    <img className="image-deco" src='../../Collectibles.png'></img>
-                </a>
-            </section>
-            <section>
-            <a href="https://github.com/Kumoko8/Collectibles-Full-Stack-App.git"target="_blank"><button>Collectibles</button></a>
             </section>
             <section>
             <a href="https://kumoko8.github.io/Weather-App-Serverside-API/"target="_blank">
