de5d6550b9045c9b9f97ed4a72c8efa89ce9e439
Kumori <tauen8@gmail.com>
2023-11-16 16:05:06 -0600

    reorganized portfolio items

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,14 +8,6 @@ export default function Portfolio() {
             </div>
             <div> <h3>Click each image to view the app!</h3></div>
             <section>
-                <a href="../../Fitness-Tracker.pdf" target="_blank">
-                    <img className="image-deco" src='../../running.png'></img>
-                </a>
-            </section>
-            <section>
-            <button>Fitness Tracker Design</button>
-            </section>
-            <section>
                 <a href="https://kumoko8.github.io/Front-End-App-DnD-Character-Project/" target="_blank">
                     <img className="image-deco" src='../../DnD.png'></img>
                 </a>
@@ -24,22 +16,23 @@ export default function Portfolio() {
             <a href="https://github.com/Kumoko8/Front-End-App-DnD-Character-Project.git"target="_blank"><button>DnD Character Generator</button></a>
             </section>
             <section>
-            <a href="https://kumoko8.github.io/Weather-App-Serverside-API/"target="_blank">
-                    <img className="image-deco" src='../../WeatherApp.png'></img>
+                <a href="https://invizproductivity4.wordpress.com/" target="_blank">
+                    <img className="image-deco" src='../../NVZlogo.png'></img>
                 </a>
             </section>
             <section>
-            <a href="https://github.com/Kumoko8/Weather-App-Serverside-API.git"target="_blank"><button>Weather App</button></a>
+          <button>WordPress Site</button>
             </section>
-            
+           
             <section>
-            <a href="https://kumoko8.github.io/Web-APIs-Quiz-Homework-4/"target="_blank">
-                    <img className="image-deco" src='../../quiz.png'></img>
+                <a href="../../Fitness-Tracker.pdf" target="_blank">
+                    <img className="image-deco" src='../../running.png'></img>
                 </a>
             </section>
             <section>
-            <a href="https://github.com/Kumoko8/Web-APIs-Quiz-Homework-4.git"target="_blank"><button>JavaScript Quiz</button></a>
+            <button>Fitness Tracker Design</button>
             </section>
+    
            
             
         </div>
