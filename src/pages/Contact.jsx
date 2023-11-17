c8ec55256bea8d720eca369d5980beaf0bc11810
Kumori <tauen8@gmail.com>
2023-11-16 18:22:54 -0600

    added images for contact and resume

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,10 +9,12 @@ export default function Contact() {
                 <h2> Contact</h2>
             </div>
             <div>
-                <ContactForm ></ContactForm>
+                <img class="image-deco"src="../Kumo_Writes.png"></img>
             </div>
+            <a href="https://share.hsforms.com/1uOdn7b8wR7yy4xUNUmEeLAqfs72" target="_blank"><h3>Message Me!</h3></a>
+            <a href="https://meetings.hubspot.com/taylor-golden" target="_blank"><h3> Set Up a Time to Chat! </h3> </a>
 
-         
+
         </div>
     )
 };
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,10 +3,13 @@ import Grid from '@mui/material/Grid';
 function Resume() {
     return (
       <Grid container spacing={2} justifyContent="center" >
-        <Grid item xs={12} margin="5em" >
+        <Grid item xs={12} margin="3em" >
+          <div >
+            <img id="kiiro-light-img"src="../kiiro-lite.PNG"></img>
+          </div>
           <div>
             <a id="resume" href="../../ResumeGeneral2.pdf" target="_blank" >
-              Resume
+              View My Resume
             </a>
           </div>
         </Grid>
