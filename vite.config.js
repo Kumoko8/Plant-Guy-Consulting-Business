6d0dc5c4ddda37ef245c8545c1efabf27665ae14
Kumori <tauen8@gmail.com>
2023-12-03 10:26:12 -0600

    deleted some unnecessary files

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { handleContactFormSubmission } from './api/contact'; // Import your route handler
 
 import react from '@vitejs/plugin-react'
 
@@ -7,17 +6,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    middleware: [
-      // Define your API routes here
-      (req, res, next) => {
-        if (req.url === '/api/contact' && req.method === 'POST') {
-          handleContactFormSubmission(req, res);
-
-        } else {
-          next();
-        }
-      },
-    ],
     port: 3000,
     open: true,
   },
