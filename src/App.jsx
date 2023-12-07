28551a16d03e3cb6893cf96721f0675b6b5324d4
Kumori <tauen8@gmail.com>
2023-12-07 14:10:37 -0600

    going to try a different approach with media query

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import AppFooter from './modules/views/AppFooter';
+
+
 import AppAppBar from './modules/views/AppAppBar';
-import myTheme from './modules/theme.ts'; // Rename the imported theme
+
 
 function App() {
-  const isSmallScreen = useMediaQuery((theme) => myTheme.breakpoints.down('sm')); // Use the renamed theme
-
+  // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <div>
+
       <Grid container>
         <Grid item xs={12}>
-          <AppAppBar />
+          <AppAppBar/>
         </Grid>
       </Grid>
-      <main className={isSmallScreen ? "mx-1" : "mx-3"}>
-        {/* Adjust the className based on screen size */}
+      <main className="mx-3">
         <Outlet />
       </main>
       <AppFooter />
     </div>
+  
+
+    //jsx fragments
   );
 }
+
+
 
 export default App;
